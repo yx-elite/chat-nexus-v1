0e888bef237d1495319f8b6e8c8b7413464ad66e
Yi Xian <exian0716@gmail.com>
2023-10-07 19:24:01 +0800

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
 def main():
-    st.header('🤖 ChatNexus v1.0')
-    st.
+    st.title('🤖 ChatNexus v1.0')
+    st.caption('Developed by: Yi Xian (yx-elite)')
+    st.write('✨ Advanced Features: WebSearch, Image Generation')
     
-    
+    # Allow prompt input
+    user_prompt = st.text_input(
+        'Ask me anything... (Shift + Enter = Line Break)',
+        
+    )
     
 if __name__ == '__main__':
     main()
