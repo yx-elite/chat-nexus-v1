4b88bb847f964a5e8304f49b3ef6155c08af0374
Yi Xian <exian0716@gmail.com>
2023-10-10 00:49:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import streamlit as st
 
 def main():
+    # Load custom css
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
+    # Set app info
     st.title('CHAT NEXUS v1.0')
     st.caption('Developed by: Yi Xian (yx-elite)')
     st.write('✨ Advanced Features: WebSearch, Image Generation')
     
-    # Allow prompt input
+    # Initialize placeholders for prompt and chat
     prompt_placeholder = st.form('chat-form')
     chat_placeholder = st.container()
     
     with prompt_placeholder:
+        # Section header
         st.markdown("**User's Prompt**")
+        
+        # User prompt input field
         cols = st.columns((6, 1))
         user_prompt = cols[0].text_input(
-            "User's Prompt",
+            'User Prompt',
             value='',
             max_chars=100,
             placeholder='Ask me anything... (Press Enter to Submit)',
-            key='input',
             label_visibility='collapsed'
         )
-        
         submit_clicked = cols[1].form_submit_button('Submit', type='primary')
-        
+    
+    # Handle actions upon button clicked    
     if submit_clicked:
         with chat_placeholder:
             if 'search' in user_prompt:
