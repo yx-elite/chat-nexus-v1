00aab8f4ea36adb8ca45dd2bf5fbe292d759fa8b
Yi Xian <exian0716@gmail.com>
2023-10-09 23:50:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 def main():
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -11,28 +10,28 @@ def main():
     
     # Allow prompt input
     prompt_placeholder = st.form('chat-form')
+    chat_placeholder = st.container()
+    
     with prompt_placeholder:
         st.markdown("**User's Prompt**")
-        cols = st.columns((6,1))
+        cols = st.columns((6, 1))
         user_prompt = cols[0].text_input(
-            "User's Prompt", 
+            "User's Prompt",
             value='',
             max_chars=100,
             placeholder='Ask me anything... (Press Enter to Submit)',
             key='input',
             label_visibility='collapsed'
         )
-        submit_btn = cols[1].form_submit_button(
-            'Submit',
-            type='primary',
-            on_click=lambda: on_click_callback(user_prompt),
-        )
-
-def on_click_callback(user_prompt):
-    # Process user's prompt
-    if 'search' in user_prompt:
-        with st.spinner('Nexus AI is now searching the web...'):
-            st.write('Searching...')
+        
+        submit_clicked = cols[1].form_submit_button('Submit', type='primary')
+        
+    if submit_clicked:
+        with chat_placeholder:
+            if 'search' in user_prompt:
+                with st.spinner('Nexus AI is now searching the web...'):
+                    st.write('Searching...')
+                    print('Searching')
     
 if __name__ == '__main__':
     main()
