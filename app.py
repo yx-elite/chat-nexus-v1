b8a30a0992c6ddbfdca7df1210bd18a4642f9ce1
Yi Xian <exian0716@gmail.com>
2023-10-10 00:50:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ def main():
     # Load custom css
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
+
     # Set app info
     st.title('CHAT NEXUS v1.0')
     st.caption('Developed by: Yi Xian (yx-elite)')
     st.write('✨ Advanced Features: WebSearch, Image Generation')
-    
+
     # Initialize placeholders for prompt and chat
     prompt_placeholder = st.form('chat-form')
     chat_placeholder = st.container()
-    
+
     with prompt_placeholder:
         # Section header
         st.markdown("**User's Prompt**")
-        
+
         # User prompt input field
         cols = st.columns((6, 1))
         user_prompt = cols[0].text_input(
@@ -28,14 +28,19 @@ def main():
             label_visibility='collapsed'
         )
         submit_clicked = cols[1].form_submit_button('Submit', type='primary')
-    
+
     # Handle actions upon button clicked    
     if submit_clicked:
-        with chat_placeholder:
-            if 'search' in user_prompt:
-                with st.spinner('Nexus AI is now searching the web...'):
-                    st.write('Searching...')
-                    print('Searching')
-    
+        handle_button_click(user_prompt, chat_placeholder)
+
+
+# Button click function
+def handle_button_click(user_prompt, chat_placeholder):
+    with chat_placeholder:
+        if 'search' in user_prompt:
+            with st.spinner('Nexus AI is now searching the web...'):
+                st.write('Searching...')
+                print('Searching')
+
 if __name__ == '__main__':
     main()
