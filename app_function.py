ed47f5ba2c6d96cbfcb8687c3c514d0303872e86
Yi Xian <exian0716@gmail.com>
2023-10-13 17:23:52 +0800

    Add api endpoint function

--- app_function.py
+++ app_function.py
@@ -2,6 +2,12 @@ import os
 import openai
 from dotenv import load_dotenv
 
+def load_api_base_url():
+    # Load environment variables
+    load_dotenv()
+    # Get the API endpoint
+    api_base_url = os.getenv("OPENAI_API_BASE")
+    return api_base_url
 
 def load_api_key():
     # Load environment variables
@@ -14,8 +20,10 @@ def load_api_key():
 def chat_connection(user_prompt, max_token=500, temperature=0.8):
     # Load the API key
     api_key = load_api_key()
+    api_base_url = load_api_base_url()
     # Set the API key for OpenAI
     openai.api_key = api_key
+    openai.api_base = api_base_url
     gpt_response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
